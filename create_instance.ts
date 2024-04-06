0684ed46868b479e922fd0789f4ffcdaedacbc74
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-06 09:20:36 +0800

    Update create_instance

--- create_instance.ts
+++ create_instance.ts
@@ -1,7 +1,7 @@
 import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 
 const main = async () => {
-  const aptosConfig = new AptosConfig({ network: Network.TESTNET });
+  const aptosConfig = new AptosConfig({ network: Network.DEVNET });
   const aptos = new Aptos(aptosConfig);
 
   console.log(aptos);
